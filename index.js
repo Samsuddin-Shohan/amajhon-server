74fd8523d0557e1619e497ace7e4d2b5a49b7ee5
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-06-14 22:05:21 +0600

    remote change

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const res = require("express/lib/response");
+
 const PORT = process.env.port || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/',async(req,res)=>{
-    res.send('hello bruh');
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.clkdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+    const database = client.db("online-store");
+    const productsCollection = database.collection("products");
+    //GET API
+    app.get("/products", async (req, res) => {
+      console.log(req.query);
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
 
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
-})
+    
+      const cursors = productsCollection.find({});
+      const count = await cursors.count();
+     // console.log(count);
+
+      let products;
+      if(page){
+        products = await cursors.skip(page*size).limit(size).toArray();
+      }
+      else{
+        products = await cursors.toArray();
+      }
+
+      res.send({
+        count,
+        products,
+      });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("hello bruh");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
